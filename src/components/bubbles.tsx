2e7b706ee0679f9dfa260bc220e4f6d64f25782c
Andrey Starostin <andrey.starostin1@yandex.ru>
2024-03-26 20:39:06 +0300

    changed bubblels position

--- src/components/bubbles.tsx
+++ src/components/bubbles.tsx
@@ -17,11 +17,11 @@ const Bubbles = () => {
 
 	return (
 		<div className="absolute top-0 left-0 h-full w-full z-10 sm:-z-30">
-			<Bubble size={68} top={10} left={20 - shift} icon={car}/>
+			<Bubble size={68} top={3} left={20 - shift} icon={car}/>
 			<Bubble size={54} top={46} left={30 - shift} icon={hamburger}/>
 			<Bubble size={58} top={38} left={65 + shift} icon={buildings}/>
 			<Bubble size={58} top={12} left={75 + shift} icon={cart}/>
-			<Bubble size={58} top={83} left={72 + shift} icon={person}/>
+			<Bubble size={58} top={83} left={80 + shift} icon={person}/>
 		</div>
 	)
 }
