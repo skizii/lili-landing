7a22c5a63d3112878ed1a1cae24d9ff96689b25f
Mikhail Tsymlov <mtsymlov@yandex.ru>
2024-04-26 16:15:08 +0200

    chore: rollback static pages

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,9 @@
-import { useTranslations } from 'next-intl'
+import { useTranslations, useLocale } from 'next-intl'
 // import { generateStaticParams } from '@/app/[locale]/layout';
 
 const Footer = () => {
   const t = useTranslations('Footer')
+  const locale = useLocale()
 
   return (
     <footer
@@ -10,7 +11,7 @@ const Footer = () => {
 		w-full xl:max-w-[1200px] px-[10px] h-[46px]
 		left-1/2 -translate-x-1/2
 		absolute bottom-0
-		px-1 sm:px-[10px] 
+		px-1 sm:px-[10px]
 		flex justify-between items-center
 		backdrop-blur-[21px]
 		lg:backdrop-blur-none
@@ -19,10 +20,10 @@ const Footer = () => {
 		z-50'>
       <p className='mb-0'>&copy; {t('copyright')}</p>
       <div className='flex gap-[10px] flex-wrap justify-center'>
-        <a className='sm:mr-[24px]' href={`/privacy-policy`}>
+        <a className='sm:mr-[24px]' href={`/${locale}/privacy-policy`}>
           {t('privacy-policy')}
         </a>
-        <a className='sm:mr-[24px]' href={`/terms-and-conditions`}>
+        <a className='sm:mr-[24px]' href={`/${locale}/terms-and-conditions`}>
           {t('terms-and-conditions')}
         </a>
         <a href='mailto:mtsymlov@gmail.com'>mtsymlov@gmail.com</a>
