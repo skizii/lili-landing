54ed2d55c73872ef3f33bdbd670f65bec2e0cab9
Mikhail Tsymlov <mtsymlov@yandex.ru>
2024-04-26 16:29:51 +0200

    chore: redirect for outdated pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/privacy-policy',
+        destination: '/en/privacy-policy',
+        permanent: true
+      },
+      {
+        source: '/terms-and-conditions',
+        destination: '/en/terms-and-conditions',
+        permanent: true
+      }
+    ]
+  },
+}
 
 export default withNextIntl(nextConfig)
