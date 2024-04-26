bea5c9f155785de51e2bac9bf2deef846f5d7a89
Mikhail Tsymlov <mtsymlov@yandex.ru>
2024-04-26 16:07:52 +0200

    chore: add inner pages translations

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,7 +1,9 @@
 const Title = ({children}: {children: string}) => {
 	return (
 		<h1 className="
-		text-center text-[40px] sm:text-[44px] lg:text-[75px] leading-[110%] mt-[42px] lg:mt-[62px] font-semibold z-20">
+		text-center text-[40px] sm:text-[44px] lg:text-[75px] leading-[110%] mt-[42px] lg:mt-[62px] font-semibold z-20"
+		style={{ whiteSpace: 'pre-line' }}
+		>
 			{children}
 		</h1>
 	)
