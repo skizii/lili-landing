9262af1d2726ee07df2220fd3b48f49ce1e91ac2
Mikhail Tsymlov <mtsymlov@yandex.ru>
2024-04-26 16:07:52 +0200

    chore: add inner pages translations

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 
 import "../globals.css";
 
@@ -13,36 +14,47 @@ const plus_jakarta_sans = Plus_Jakarta_Sans({
   variable: '--font-plus-jakarta-sans'
 });
 
-export const metadata = {
-  title: "Lili: Expense & Money Tracker",
-  description: "Explore and analyze your budget with a very intuitive interface",
-  icons: {
-    icon: 'icons/icon.svg',
-    apple: 'icons/apple-icon.png',
-    other: {
-      rel: 'icon',
-      url: 'icons/favicon.ico',
+export async function generateMetadata({
+ params: {locale}
+}: Omit<Props, 'children'>) {
+  const t = await getTranslations({locale, namespace: 'LocaleLayout'});
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    icons: {
+      icon: 'icons/icon.svg',
+      apple: 'icons/apple-icon.png',
+      other: {
+        rel: 'icon',
+        url: 'icons/favicon.ico',
+      }
     }
-  },
+  };
+}
+
+type Props = {
+  children: React.ReactNode;
+  params: {locale: string};
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function LocaleLayout({
+   children,
+   params: {locale}
+ }: Props) {
+
   const messages = useMessages();
 
   return (
     <NextIntlClientProvider messages={messages}>
-      <html lang="en">
-        <head>
-          <YMetricaHead/>
-        </head>
-        <Suspense>
-          <YandexMetrika/>
-        </Suspense>
-        <body className={`${plus_jakarta_sans.variable} bg-main bg-bottom bg-net-pattern bg-no-repeat bg-repeat-x text-white`}>{children}</body>
+      <html lang={locale}>
+      <head>
+        <YMetricaHead/>
+      </head>
+      <Suspense>
+        <YandexMetrika/>
+      </Suspense>
+      <body className={`${plus_jakarta_sans.variable} bg-main bg-bottom bg-net-pattern bg-no-repeat bg-repeat-x text-white`}>{children}</body>
       </html>
     </NextIntlClientProvider>
   );
